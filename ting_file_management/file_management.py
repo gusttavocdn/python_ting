a06c18bf85804496bfcb553cb0dc66356192fec1
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-02-12 14:53:16 -0300

    feat: implement file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, 'r') as file:
+            content_list = list()
+            for line in file:
+                content_list.append(line.strip())
+        return content_list
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
